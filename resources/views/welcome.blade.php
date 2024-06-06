a1043730a66d26c8c05c30d06f6db8b3ad9a4cea
Oliver Gomez <kayserenrique@gmail.com>
2024-06-06 10:39:30 -0400

    redirect

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Vevpoker</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -16,6 +16,7 @@
         </style>
     </head>
     <body class="font-sans antialiased dark:bg-black dark:text-white/50">
+        <!--
         <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
             <img id="background" class="absolute -left-20 top-0 max-w-[877px]" src="https://laravel.com/assets/img/welcome/background.svg" />
             <div class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
@@ -168,5 +169,30 @@
                 </div>
             </div>
         </div>
+        -->
+        <section class="pb-12 bg-gradient-to-b from-[#E8E3F5] via-[#EDEAFB] to-[#F7FAFC]">
+            <div class="items-center pt-12 px-8 mx-auto max-w-7xl lg:px-16 md:px-12">
+              <div class="justify-center w-full text-center lg:p-10 max-auto">
+                <div class="justify-center w-full mx-auto">
+
+                    <div class="flex flex-col items-center justify-center max-w-xl gap-3 mx-auto lg:flex-row">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 w-64 h-64">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M11.42 15.17 17.25 21A2.652 2.652 0 0 0 21 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 1 1-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 0 0 4.486-6.336l-3.276 3.277a3.004 3.004 0 0 1-2.25-2.25l3.276-3.276a4.5 4.5 0 0 0-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437 1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008Z" />
+                        </svg>
+                    </div>
+
+                    <h2 class="text-6xl font-semibold text-black dark:text-white">Bienvenido</h2>
+
+                    <p class="mt-4 text-sm/relaxed">
+                        Estamos realizando mejoras al sitio web disculpa las molestias y los problemas causados
+                    </p>
+
+                </div>
+              </div>
+          
+            </div>      
+
+        </section> 
+
     </body>
 </html>
